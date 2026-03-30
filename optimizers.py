13a6e55cbd47dcd0c6e4794e08b7bda4ce5939be
Natalie Lysenko <lysenko.nataliiia@gmail.com>
2026-03-30 10:40:09 -0600

    add optimizer.py

---
+++ optimizers.py
@@ -0,0 +1,5 @@
+"""
+optimizers.py — optimization algorithms, and cross-validation framework .
+
+Import what you need in any notebook: e.g., from optimizers import Adam, SGD, grid_search_cv, final_evaluate
+"""
