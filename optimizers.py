14b15f44f76a5201aa9c642e3876c884167b9307
HAONAN MA <63064892+Alex-god281@users.noreply.github.com>
2026-03-31 08:05:26 +0800

    Delete optimizers.py

--- optimizers.py
+++
@@ -1,5 +0,0 @@
-"""
-optimizers.py — optimization algorithms, and cross-validation framework .
-
-Import what you need in any notebook: e.g., from optimizers import Adam, SGD, grid_search_cv, final_evaluate
-"""
